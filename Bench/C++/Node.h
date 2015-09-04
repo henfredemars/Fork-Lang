5042fc703d35eac3605f7121ebb82eb572c5f762
henfredemars <henfredemars@users.noreply.github.com>
2015-09-04 10:13:16 -0400

    Commiting bench files that haven't been pushed in yet for C++ bench

---
+++ Bench/C++/Node.h
@@ -0,0 +1,29 @@
+// Node definition and implementation for C++ binary tree benchmark program
+
+#ifndef NODE_H
+#define NODE_H
+
+#include <random>
+#include <math.h>
+#include "gc.h"
+#include "gc_cpp.h"
+#include "util.h"
+
+#define WORK_COST 10000
+
+class Node : public gc {
+  private:
+    double value;
+    Node* leftchild;
+    Node* rightchild;
+  public:
+    Node(void);
+    void populate(void);
+    void do_work(void);
+    static Node* build_tree_of_size(int size);
+    void insert_into_tree(Node* new_node);
+    static std::uniform_real_distribution<double>* generator;
+    static std::mt19937_64* rng;
+};
+
+#endif /* Node.h */
