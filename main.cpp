61229ac964a29238c85953acc7625caaadf8a0ed
Ryan Zavoral <rzavo76@ufl.edu>
2015-09-14 11:58:38 -0400

    Added file parsing capability instead of stdinput

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 #include "node.h"
+#include "stdio.h"
+
 extern Block *program;
 extern int yyparse();
+extern FILE* yyin;
 
 int main(int argc, char **argv) {
 	GC_INIT();
-	yyparse();
-	std::cout << program << std::endl;
+	if(argc == 2) {
+		yyin = fopen(argv[1], "r");
+		if(yyin) {
+			yyparse();
+			fclose(yyin);
+		}
+		else {
+			std::cout << "Error, failed to open file: " << argv[1] << "\n";
+		}
+	}
+	else if(argc > 2) {
+		std::cout << "Error, too many inputs.\n";
+	}
+	else {
+		std::cout << "Error, need file input.\n";
+	}
 	return 0;
 }
