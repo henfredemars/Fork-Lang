2f77a8e914a31b95178fd30994c88bb5fd1d75ea
henfredemars <marsrover7@fastmail.com>
2015-09-14 11:12:55 -0400

    Updated example programs to reflect new standard for entry point, added debug flags for GC, flex hooks for the garbage collector

---
+++ yy_overrides.h
@@ -0,0 +1,17 @@
+//Header include provides replacements for default flex memory allocator
+//  Scanner must NOT be reentrant for this to work!
+
+void* yyalloc(size_t bytes) {
+  //Tell collector to look inside the allocation for pointers
+  //  but do not handle collection of the memory itself
+  return GC_MALLOC_UNCOLLECTABLE(bytes);
+}
+
+void* yyrealloc(void* ptr, size_t bytes) {
+  return GC_REALLOC(ptr,bytes);
+}
+
+void yyfree(void* ptr) {
+  GC_FREE(ptr);
+}
+
