9cb383a5e5b4e2008aeeca8e673200d82bb0baea
henfredemars <marsrover7@fastmail.com>
2015-11-07 19:42:10 -0500

    Threading and parallel execution infrastructure for scheduling tasks

--- lib.cpp
+++ lib.cpp
@@ -8,6 +8,20 @@
 std::mutex print_mutex;
 std::mutex malloc_mutex;
 
+//Parallism data structures
+int64_t thread_count = 0;
+int64_t max_threads = 0;
+std::map<int,std::future<int64_t>> int_future_id_map;
+std::map<int,std::future<double>> float_future_id_map;
+std::map<int,std::future<int64_t*>> intptr_future_id_map;
+std::map<int,std::future<double*>> floatptr_future_id_map;
+std::map<int,std::future<void>> void_future_id_map;
+std::mutex thread_count_mutex;
+std::mutex map_mutex;
+
+//Public-facing standard library functions
+//Note that cmath functions (Ex: sin, cos) are available as well
+
 extern "C" void print_int(int64_t i) {
   print_mutex.lock();
   printf("Outputing Integer: %d\n",(int)i);
@@ -18,14 +32,6 @@ extern "C" void print_float(double d) {
   print_mutex.lock();
   printf("Outputing Float: %f\n",d);
   print_mutex.unlock();
-}
-
-extern "C" float reconcile_float(double d_old,double d_new,int partx, int ofn) {
-  return d_new; //Update always wins for now
-}
-
-extern "C" int64_t reconcile_int(int64_t i_old,int64_t i_new,int partx, int ofn) {
-  return i_new; //Update always wins for now
 }
 
 extern "C" float* malloc_float(int64_t s) {
@@ -68,5 +74,185 @@ extern "C" void free_int(int64_t* i) {
   malloc_mutex.unlock();
 }
 
+//Hidden funcitons implement parallism
+//They are not intended to be called by user code
 
+void set_max_threads() {
+  if (max_threads) return;
+  unsigned long dth = std::thread::hardware_concurrency();
+  printf("Detected %d compute elements.\n",(int)dth);
+  if (dth < 1) max_threads = 1;
+  else if (dth > 4) max_threads = 4;
+  else max_threads = dth;
+  printf("Setting max_threads = %d\n",(int)max_threads);
+}
+
+extern "C"  void __fork_sched_int(int64_t (*statement)(void),int64_t id) {
+  set_max_threads();
+  std::future<int64_t> promise;
+  thread_count_mutex.lock();
+  if (thread_count >= max_threads) {
+    thread_count_mutex.unlock();
+    promise = std::async(std::launch::deferred,statement);
+  } else {
+    promise = std::async(std::launch::async,statement);
+    thread_count++;
+    thread_count_mutex.unlock();
+  }
+  map_mutex.lock();
+  int_future_id_map.insert(std::pair<int64_t,std::future<int64_t>>(id,std::move(promise)));
+  map_mutex.unlock();
+}
+
+extern "C"  void __fork_sched_float(double (*statement)(void),int64_t id) {
+  set_max_threads();
+  std::future<double> promise;
+  thread_count_mutex.lock();
+  if (thread_count >= max_threads) {
+    thread_count_mutex.unlock();
+    promise = std::async(std::launch::deferred,statement);
+  } else {
+    promise = std::async(std::launch::async,statement);
+    thread_count++;
+    thread_count_mutex.unlock();
+  }
+  map_mutex.lock();
+  float_future_id_map.insert(std::pair<int64_t,std::future<double>>(id,std::move(promise)));
+  map_mutex.unlock();
+}
+
+extern "C"  void __fork_sched_intptr(int64_t* (*statement)(void),int64_t id) {
+  set_max_threads();
+  std::future<int64_t*> promise;
+  thread_count_mutex.lock();
+  if (thread_count >= max_threads) {
+    thread_count_mutex.unlock();
+    promise = std::async(std::launch::deferred,statement);
+  } else {
+    promise = std::async(std::launch::async,statement);
+    thread_count++;
+    thread_count_mutex.unlock();
+  }
+  map_mutex.lock();
+  intptr_future_id_map.insert(std::pair<int64_t,std::future<int64_t*>>(id,std::move(promise)));
+  map_mutex.unlock();
+}
+
+extern "C"  void __fork_sched_floatptr(double* (*statement)(void),int64_t id) {
+  set_max_threads();
+  std::future<double*> promise;
+  thread_count_mutex.lock();
+  if (thread_count >= max_threads) {
+    thread_count_mutex.unlock();
+    promise = std::async(std::launch::deferred,statement);
+  } else {
+    promise = std::async(std::launch::async,statement);
+    thread_count++;
+    thread_count_mutex.unlock();
+  }
+  map_mutex.lock();
+  floatptr_future_id_map.insert(std::pair<int64_t,std::future<double*>>(id,std::move(promise)));
+  map_mutex.unlock();
+}
+
+extern "C"  void __fork_sched_void(void (*statement)(void),int64_t id) {
+  set_max_threads();
+  std::future<void> promise;
+  thread_count_mutex.lock();
+  if (thread_count >= max_threads) {
+    thread_count_mutex.unlock();
+    promise = std::async(std::launch::deferred,statement);
+  } else {
+    promise = std::async(std::launch::async,statement);
+    thread_count++;
+    thread_count_mutex.unlock();
+  }
+  map_mutex.lock();
+  void_future_id_map.insert(std::pair<int64_t,std::future<void>>(id,std::move(promise)));
+  map_mutex.unlock();
+}
+
+extern "C" int64_t __recon_int(int64_t original,int64_t known,int64_t update,int64_t id,int64_t max) {
+  std::chrono::milliseconds span(0);
+  map_mutex.lock();
+  auto fv = std::move(int_future_id_map.at(id));
+  int_future_id_map.erase(id);
+  map_mutex.unlock();
+  if (fv.wait_for(span)==std::future_status::deferred) {
+    return fv.get();
+  } else {
+    auto v = fv.get();
+    thread_count_mutex.lock();
+    thread_count--;
+    thread_count_mutex.unlock();
+    return v;
+  }
+}
+
+extern "C" double __recon_float(double original,int64_t known,double update,int64_t id,int64_t max) {
+  std::chrono::milliseconds span(0);
+  map_mutex.lock();
+  auto fv = std::move(float_future_id_map.at(id));
+  float_future_id_map.erase(id);
+  map_mutex.unlock();
+  if (fv.wait_for(span)==std::future_status::deferred) {
+    return fv.get();
+  } else {
+    auto v = fv.get();
+    thread_count_mutex.lock();
+    thread_count--;
+    thread_count_mutex.unlock();
+    return v;
+  }
+}
+
+extern "C" int64_t* __recon_intptr(int64_t* original,int64_t known,int64_t* update,int64_t id,int64_t max) {
+  std::chrono::milliseconds span(0);
+  map_mutex.lock();
+  auto fv = std::move(intptr_future_id_map.at(id));
+  intptr_future_id_map.erase(id);
+  map_mutex.unlock();
+  if (fv.wait_for(span)==std::future_status::deferred) {
+    return fv.get();
+  } else {
+    auto v = fv.get();
+    thread_count_mutex.lock();
+    thread_count--;
+    thread_count_mutex.unlock();
+    return v;
+  }
+}
+
+extern "C" double* __recon_floatptr(double* original,int64_t known,double* update,int64_t id,int64_t max) {
+  std::chrono::milliseconds span(0);
+  map_mutex.lock();
+  auto fv = std::move(floatptr_future_id_map.at(id));
+  floatptr_future_id_map.erase(id);
+  map_mutex.unlock();
+  if (fv.wait_for(span)==std::future_status::deferred) {
+    return fv.get();
+  } else {
+    auto v = fv.get();
+    thread_count_mutex.lock();
+    thread_count--;
+    thread_count_mutex.unlock();
+    return v;
+  }
+}
+
+extern "C" void  __recon_void(int64_t id,int64_t max) {
+  std::chrono::milliseconds span(0);
+  map_mutex.lock();
+  auto fv = std::move(void_future_id_map.at(id));
+  void_future_id_map.erase(id);
+  map_mutex.unlock();
+  if (fv.wait_for(span)==std::future_status::deferred) {
+    fv.get();
+  } else {
+    fv.get();
+    thread_count_mutex.lock();
+    thread_count--;
+    thread_count_mutex.unlock();
+  }
+}
 
--- lib.h
+++ lib.h
@@ -1,18 +1,20 @@
 //Fork standard library
 
+#include <cmath>
+#include <chrono>
+#include <future>
 #include <mutex>
+#include <thread>
+#include <map>
 #include <stdint.h>
 #include <stdio.h>
+#include "gc/include/gc.h"
 
 //extern "C" disables C++ name mangling
 
 extern "C" void print_int(int64_t i);
 
 extern "C" void print_float(double d);
-
-extern "C" float reconcile_float(double d_old,double d_new,int partx, int ofn);
-
-extern "C" int64_t reconcile_int(int64_t i_old,int64_t i_new,int partx, int ofn);
 
 extern "C" float* malloc_float(int64_t s);
 
@@ -26,6 +28,24 @@ extern "C" void free_float(float* f);
 
 extern "C" void free_int(int64_t* i);
 
+extern "C"  void __fork_sched_int(int64_t (*statement)(void),int64_t id);
 
+extern "C"  void __fork_sched_float(double (*statement)(void),int64_t id);
+
+extern "C"  void __fork_sched_intptr(int64_t* (*statement)(void),int64_t id);
+
+extern "C"  void __fork_sched_floatptr(double* (*statement)(void),int64_t id);
+
+extern "C"  void __fork_sched_void(void (*statement)(void),int64_t id);
+
+extern "C" int64_t __recon_int(int64_t original,int64_t known,int64_t update,int64_t id,int64_t max);
+
+extern "C" double __recon_float(double original,int64_t known,double update,int64_t id,int64_t max);
+
+extern "C" int64_t* __recon_intptr(int64_t* original,int64_t known,int64_t* update,int64_t id,int64_t max);
+
+extern "C" double* __recon_floatptr(double* original,int64_t known,double* update,int64_t id,int64_t max);
+
+extern "C" void __recon_void(int64_t id,int64_t max);
 
 
