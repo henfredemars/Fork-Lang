dc0edb50952ab9862832097f27199db04da19e2b
henfredemars <henfredemars@users.noreply.github.com>
2015-11-07 23:01:35 -0500

    Parallel execution context manager, updated binary interface, added to build system

--- lib.cpp
+++ lib.cpp
@@ -8,16 +8,8 @@
 std::mutex print_mutex;
 std::mutex malloc_mutex;
 
-//Parallism data structures
-int64_t thread_count = 0;
-int64_t max_threads = -1;
-std::map<int,std::future<int64_t>> int_future_id_map;
-std::map<int,std::future<double>> float_future_id_map;
-std::map<int,std::future<int64_t*>> intptr_future_id_map;
-std::map<int,std::future<double*>> floatptr_future_id_map;
-std::map<int,std::future<void>> void_future_id_map;
-std::mutex thread_count_mutex;
-std::mutex map_mutex;
+//Parallism manager
+ParContextManager manager;
 
 //Public-facing standard library functions
 //Note that cmath functions (Ex: sin, cos) are available as well
@@ -76,102 +68,27 @@ extern "C" void free_int(int64_t* i) {
 
 //Hidden funcitons implement parallism
 //They are not intended to be called by user code
-//Only the main thread may call these functions
-
-void set_max_threads() {
-  if (max_threads != -1) return;
-  unsigned long dth = std::thread::hardware_concurrency()-1;
-  printf("Detected %d additional compute elements.\n",(int)dth);
-  if (dth < 0) max_threads = 0;
-  else if (dth > 3) max_threads = 3;
-  else max_threads = dth;
-  printf("Setting max execution threads to: %d\n",(int)max_threads+1);
-}
 
 // id - zero-indexed number of the statement being scheduled for the commit
-extern "C"  void __fork_sched_int(int64_t (*statement)(void),int64_t id) {
-  set_max_threads();
-  std::future<int64_t> promise;
-  thread_count_mutex.lock();
-  if (thread_count >= max_threads) {
-    thread_count_mutex.unlock();
-    promise = std::async(std::launch::deferred,statement);
-  } else {
-    promise = std::async(std::launch::async,statement);
-    thread_count++;
-    thread_count_mutex.unlock();
-  }
-  map_mutex.lock();
-  int_future_id_map.insert(std::pair<int64_t,std::future<int64_t>>(id,std::move(promise)));
-  map_mutex.unlock();
+// cid - context identifier obtained from make_context
+extern "C"  void __fork_sched_int(int64_t (*statement)(void),int64_t id,int64_t cid) {
+  manager.sched_int(statement,id,cid);
 }
 
-extern "C"  void __fork_sched_float(double (*statement)(void),int64_t id) {
-  set_max_threads();
-  std::future<double> promise;
-  thread_count_mutex.lock();
-  if (thread_count >= max_threads) {
-    thread_count_mutex.unlock();
-    promise = std::async(std::launch::deferred,statement);
-  } else {
-    promise = std::async(std::launch::async,statement);
-    thread_count++;
-    thread_count_mutex.unlock();
-  }
-  map_mutex.lock();
-  float_future_id_map.insert(std::pair<int64_t,std::future<double>>(id,std::move(promise)));
-  map_mutex.unlock();
+extern "C"  void __fork_sched_float(double (*statement)(void),int64_t id,int64_t cid) {
+  manager.sched_float(statement,id,cid);
 }
 
-extern "C"  void __fork_sched_intptr(int64_t* (*statement)(void),int64_t id) {
-  set_max_threads();
-  std::future<int64_t*> promise;
-  thread_count_mutex.lock();
-  if (thread_count >= max_threads) {
-    thread_count_mutex.unlock();
-    promise = std::async(std::launch::deferred,statement);
-  } else {
-    promise = std::async(std::launch::async,statement);
-    thread_count++;
-    thread_count_mutex.unlock();
-  }
-  map_mutex.lock();
-  intptr_future_id_map.insert(std::pair<int64_t,std::future<int64_t*>>(id,std::move(promise)));
-  map_mutex.unlock();
+extern "C"  void __fork_sched_intptr(int64_t* (*statement)(void),int64_t id,int64_t cid) {
+  manager.sched_intptr(statement,id,cid);
 }
 
-extern "C"  void __fork_sched_floatptr(double* (*statement)(void),int64_t id) {
-  set_max_threads();
-  std::future<double*> promise;
-  thread_count_mutex.lock();
-  if (thread_count >= max_threads) {
-    thread_count_mutex.unlock();
-    promise = std::async(std::launch::deferred,statement);
-  } else {
-    promise = std::async(std::launch::async,statement);
-    thread_count++;
-    thread_count_mutex.unlock();
-  }
-  map_mutex.lock();
-  floatptr_future_id_map.insert(std::pair<int64_t,std::future<double*>>(id,std::move(promise)));
-  map_mutex.unlock();
+extern "C"  void __fork_sched_floatptr(double* (*statement)(void),int64_t id,int64_t cid) {
+  manager.sched_floatptr(statement,id,cid);
 }
 
-extern "C"  void __fork_sched_void(void (*statement)(void),int64_t id) {
-  set_max_threads();
-  std::future<void> promise;
-  thread_count_mutex.lock();
-  if (thread_count >= max_threads) {
-    thread_count_mutex.unlock();
-    promise = std::async(std::launch::deferred,statement);
-  } else {
-    promise = std::async(std::launch::async,statement);
-    thread_count++;
-    thread_count_mutex.unlock();
-  }
-  map_mutex.lock();
-  void_future_id_map.insert(std::pair<int64_t,std::future<void>>(id,std::move(promise)));
-  map_mutex.unlock();
+extern "C"  void __fork_sched_void(void (*statement)(void),int64_t id,int64_t cid) {
+  manager.sched_void(statement,id,cid);
 }
 
 //Description of function parameters:
@@ -180,89 +97,33 @@ extern "C"  void __fork_sched_void(void (*statement)(void),int64_t id) {
 //  update - value of previous recon statement, original if known, else any value accepted
 //  id - index of statement being reconned in this commit, starting at zero
 //  max - maximum valid index of statements in this commit
+//  cid - context identifier obtained from make_context
 //Conflict resolution scheme: none, always return value of the statement
 //Right now, none of the function parameters are used
-extern "C" int64_t __recon_int(int64_t original,int64_t known,int64_t update,int64_t id,int64_t max) {
-  std::chrono::milliseconds span(0);
-  map_mutex.lock();
-  auto fv = std::move(int_future_id_map.at(id));
-  int_future_id_map.erase(id);
-  map_mutex.unlock();
-  if (fv.wait_for(span)==std::future_status::deferred) {
-    return fv.get();
-  } else {
-    auto v = fv.get();
-    thread_count_mutex.lock();
-    thread_count--;
-    thread_count_mutex.unlock();
-    return v;
-  }
+extern "C" int64_t __recon_int(int64_t original,int64_t known,int64_t update,int64_t id,int64_t max,int64_t cid) {
+  return manager.recon_int(original,known,update,id,max,cid);
 }
 
-extern "C" double __recon_float(double original,int64_t known,double update,int64_t id,int64_t max) {
-  std::chrono::milliseconds span(0);
-  map_mutex.lock();
-  auto fv = std::move(float_future_id_map.at(id));
-  float_future_id_map.erase(id);
-  map_mutex.unlock();
-  if (fv.wait_for(span)==std::future_status::deferred) {
-    return fv.get();
-  } else {
-    auto v = fv.get();
-    thread_count_mutex.lock();
-    thread_count--;
-    thread_count_mutex.unlock();
-    return v;
-  }
+extern "C" double __recon_float(double original,int64_t known,double update,int64_t id,int64_t max,int64_t cid) {
+  return manager.recon_float(original,known,update,id,max,cid);
 }
 
-extern "C" int64_t* __recon_intptr(int64_t* original,int64_t known,int64_t* update,int64_t id,int64_t max) {
-  std::chrono::milliseconds span(0);
-  map_mutex.lock();
-  auto fv = std::move(intptr_future_id_map.at(id));
-  intptr_future_id_map.erase(id);
-  map_mutex.unlock();
-  if (fv.wait_for(span)==std::future_status::deferred) {
-    return fv.get();
-  } else {
-    auto v = fv.get();
-    thread_count_mutex.lock();
-    thread_count--;
-    thread_count_mutex.unlock();
-    return v;
-  }
+extern "C" int64_t* __recon_intptr(int64_t* original,int64_t known,int64_t* update,int64_t id,int64_t max,int64_t cid) {
+  return manager.recon_intptr(original,known,update,id,max,cid);
 }
 
-extern "C" double* __recon_floatptr(double* original,int64_t known,double* update,int64_t id,int64_t max) {
-  std::chrono::milliseconds span(0);
-  map_mutex.lock();
-  auto fv = std::move(floatptr_future_id_map.at(id));
-  floatptr_future_id_map.erase(id);
-  map_mutex.unlock();
-  if (fv.wait_for(span)==std::future_status::deferred) {
-    return fv.get();
-  } else {
-    auto v = fv.get();
-    thread_count_mutex.lock();
-    thread_count--;
-    thread_count_mutex.unlock();
-    return v;
-  }
+extern "C" double* __recon_floatptr(double* original,int64_t known,double* update,int64_t id,int64_t max,int64_t cid) {
+  return manager.recon_floatptr(original,known,update,id,max,cid);
 }
 
-extern "C" void  __recon_void(int64_t id,int64_t max) {
-  std::chrono::milliseconds span(0);
-  map_mutex.lock();
-  auto fv = std::move(void_future_id_map.at(id));
-  void_future_id_map.erase(id);
-  map_mutex.unlock();
-  if (fv.wait_for(span)==std::future_status::deferred) {
-    fv.get();
-  } else {
-    fv.get();
-    thread_count_mutex.lock();
-    thread_count--;
-    thread_count_mutex.unlock();
-  }
+extern "C" void  __recon_void(int64_t id,int64_t max,int64_t cid) {
+  manager.recon_void(id,max,cid);
 }
 
+extern "C" int64_t __make_context() {
+  return manager.make_context();
+}
+
+extern "C" void __destroy_context(int64_t cid) {
+  manager.destroy_context(cid);
+}
